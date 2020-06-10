8f2feaf4efd9b5827a101811ae2bd4a8f90d24c3
Henry Foster <hfoster@mitre.org>
2020-06-10 13:40:16 -0400

    move docs to plugin from core

---
+++ README.md
@@ -0,0 +1,3 @@
+# Caldera: The Field Manual
+
+A Caldera plugin for generating and serving Caldera documentation.
---
+++ hook.py
@@ -0,0 +1,34 @@
+import asyncio
+import pathlib
+
+from aiohttp import web
+
+from app.utility.base_world import BaseWorld
+
+name = 'fieldmanual'
+description = 'Holds and serves Caldera documentation'
+address = '/plugin/%s/gui' % (name,)
+access = BaseWorld.Access.APP
+
+plugin_root = pathlib.Path('plugins/%s' % (name,))
+sphinx_docs_root = plugin_root / 'sphinx-docs'
+html_docs_root = sphinx_docs_root / '_build' / 'html'
+
+
+async def landing(_):
+    return web.FileResponse(str(plugin_root / 'static' / 'opener.html'))
+
+
+async def build_docs():
+    process = await asyncio.create_subprocess_exec(
+        'sphinx-build', 'sphinx-docs/', str(html_docs_root), '-b', 'html', '-c', str(sphinx_docs_root),
+        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    await process.communicate()
+
+
+async def enable(services, loop=None):
+    loop = loop if loop else asyncio.get_event_loop()
+    loop.create_task(build_docs())
+    app_svc = services.get('app_svc')
+    app_svc.application.router.add_route('GET', address, landing)
+    app_svc.application.router.add_static('/docs/', str(html_docs_root.absolute()), append_version=True)
